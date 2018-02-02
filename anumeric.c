cd2f7fa7d825222dada88869eb744a5c367ffcf4
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-02 16:50:19 +0530

    anumeric.c
    to print the output

---
+++ anumeric.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[20]="hello123";
+printf("enter te string "):
+for(i=0;s[i]!/=0;i++)
+{
+if(a[i]="1";a[i]="2",a[i]="3",a[i]="4",a[i]="5",a[i]="6",a[i]="7",a[i]="8",a[i]="9")
+{
+ count++;
+ }
+ printf("enter the count");
+ }
+ }
+ 
