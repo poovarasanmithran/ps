b798dbdfccc56c04c65e60458016ad25f15ea40b
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-02 16:27:17 +0530

    para.c
    to print the lines in a paragraph

---
+++ para.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#incude<string.h>
+void main()
+{
+char s[500];
+int count 0,i;
+printf("enter the string");
+scanf("%d",'.');
+for(i=0;s[i]!=0;i++)
+{
+if(s[i]==1)
+{
+ count++;
+ }
+ printf("no of lines in given paragraph");
+ }
+ 
