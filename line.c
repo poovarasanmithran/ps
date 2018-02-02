6f7559b8a5b7abb5fff3abde71378af23ca25c7a
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-02 16:22:26 +0530

    line.c
    to print the line values

---
+++ line.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+main()
+{
+char str[500];
+int i=0;count =0;
+printf("enter the string\n");
+while(str[i]!='\0')
+{
+if(str[i]=='')
+count++;
+}
+printf("number of words in the string %d\n",count+1);
+}
